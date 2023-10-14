fe1fce60107d2c9d3ccc1dfb40881d82ba2d83aa
Leandro Leme dos Santos <leandrolemesantos@gmail.com>
2023-10-14 18:23:26 -0300

    278º dia de CTD - HTML + JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,23 @@
-var newspaper = 0;
+function typeWriter(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 110 * i);
+    });
+  }
+
+  const titulo = document.querySelector('h1');
+  typeWriter(titulo);
+
+  //* animação 'p'*//
+
+  function typeWriter2(elemento) {
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach((letra, i) => {
+      setTimeout(() => elemento.innerHTML += letra, 18 * i);
+    });
+  }
+
+  const subtitulo = document.querySelector('p');
+  typeWriter2(subtitulo);
