a4ebaa757813f5f1d3ebcbc7519aa7568bc13b6c
Leandro Leme dos Santos <leandrolemesantos@gmail.com>
2023-10-15 21:39:17 -0300

    279º dia de CTD - CSS + JS.

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,16 +8,3 @@ function typeWriter(elemento) {
 
   const titulo = document.querySelector('h1');
   typeWriter(titulo);
-
-  //* animação 'p'*//
-
-  function typeWriter2(elemento) {
-    const textoArray = elemento.innerHTML.split('');
-    elemento.innerHTML = '';
-    textoArray.forEach((letra, i) => {
-      setTimeout(() => elemento.innerHTML += letra, 18 * i);
-    });
-  }
-
-  const subtitulo = document.querySelectorAll('p');
-  typeWriter2(subtitulo);
